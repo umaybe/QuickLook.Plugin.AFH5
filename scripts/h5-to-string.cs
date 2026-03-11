0322cded6c60dc84d7e94baf213dacc89a9c4355
umaybe <1524477126@qq.com>
2026-03-11 23:56:52 +0800

    add a script to extract the scheme strings of a cas.h5 file to multiple files

---
+++ scripts/h5-to-string.cs
@@ -0,0 +1,27 @@
+// A script to extract the scheme strings of a cas.h5 file to multiple files.
+// usage: dotnet h5-to-string.cs <path to cas.h5>
+// You need .net 10 or later to run this.
+
+#:package PureHDF@1.0.1
+
+using PureHDF;
+
+var file = H5File.OpenRead(args[0]);
+var settings = file.Group("/settings");
+var datasets = new string[]
+{
+    "Cortex Variables",
+    "Domain Variables",
+    "Origin",
+    "Rampant Variables",
+    "Solver",
+    "TGrid Variables",
+    "Thread Variables",
+    "Version"
+};
+
+foreach (var dataset in datasets)
+{
+    var data = settings.Dataset(dataset).Read<string>();
+    System.IO.File.WriteAllText($"{dataset}.scm", data, System.Text.Encoding.UTF8);
+}
